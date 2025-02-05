7421a620bca8080accb2d5f1ab8b1dbf1d70f7ca
SyedGMIbrahim <syedgibrahim@gmail.com>
2025-02-05 17:24:58 +0530

    Backend code updated

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+const JWT_SECRET = syedIbrahim29
+
+module.export = JWT_SECRET;
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,13 @@
 const express = require("express");
+const mainRouter = require("./routes/index");
+const {User} = require("./db");
+const cors = require("cors");
+
+const app = express();
+
+app.use("/api/v1", mainRouter);
+app.use(cors())
+app.use(express.json());
 
 
+app.listen(3000);
