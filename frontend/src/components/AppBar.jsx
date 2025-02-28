833633ab0a895557886f90e94a51646e50d1c193
SyedGMIbrahim <syedgibrahim@gmail.com>
2025-02-28 08:05:01 +0530

    Frontend code with bugs

---
+++ frontend/src/components/AppBar.jsx
@@ -0,0 +1,17 @@
+export function AppBar() {
+    return <div className="shadow h-16 flex justify-between pt-1">
+        <div className="flex flex-col justify-center h-full pl-4">
+            PayTM App
+        </div>
+        <div className="flex pr-4">
+            <div className="flex flex-col justify-center h-full mr-4">
+                Hello
+            </div>
+            <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
+                <div className="flex flex-col justify-center h-full text-xl">
+                    U
+                </div>
+            </div>
+        </div>
+    </div>
+}
